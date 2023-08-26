e38c5facd951b54ab4783b256810b7ac15839cf9
dinod <dinodtharinda2001@gmail.com>
2023-08-26 22:47:42 +0530

    new changes

--- src/data_structures/TaskNode.java
+++ src/data_structures/TaskNode.java
@@ -1,16 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package data_structures;
 
-/**
- *
- * @author dinod
- */
+package data_structures;
+import java.time.LocalDateTime;
+
 public class TaskNode {
     String name;
-    String date;
+    LocalDateTime startDate;
+    LocalDateTime endDate;
     String status;
     TaskNode nextTask;
 }
--- src/data_structures/taskLinkedList.java
+++ src/data_structures/taskLinkedList.java
@@ -1,11 +1,54 @@
 package data_structures;
 
+import java.time.LocalDateTime;
+
 public class taskLinkedList {
 
     TaskNode head;
 
-    public void insertBegin(String name, String date, String status) {
-        
+    public void insertBegin(String name, LocalDateTime startDate, LocalDateTime endDate, String status) {
+        if (head == null) {
+            TaskNode newTask = new TaskNode();
+            newTask.name = name;
+            newTask.endDate = endDate;
+            newTask.status = status;
+
+        } else {
+            TaskNode newTask = new TaskNode();
+            newTask.name = name;
+            newTask.endDate = endDate;
+            newTask.startDate = startDate;
+            newTask.status = status;
+            newTask.nextTask = head;
+            head = newTask;
+        }
+    }
+
+    public void insertEnd(String name, LocalDateTime startDate, LocalDateTime endDate, String status) {
+        if (head == null) {
+            TaskNode newTask = new TaskNode();
+            newTask.name = name;
+            newTask.endDate = endDate;
+            newTask.status = status;
+
+        } else {
+            TaskNode lastTask = head;
+            while (lastTask.nextTask != null) {
+                lastTask = lastTask.nextTask;
+
+            }
+            TaskNode newTask = new TaskNode();
+            newTask.name = name;
+            newTask.startDate = startDate;
+            newTask.endDate = endDate;
+            newTask.status = status;
+            lastTask.nextTask = newTask;
+
+        }
+    }
+
+    public void insertAfter(String name, LocalDateTime startDate, LocalDateTime endDate, String status, TaskNode tast) {
+
     }
 
 }
